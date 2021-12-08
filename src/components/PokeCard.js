6fa8a3ca581025feb37cd3aba5f646cb6f3b6571
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-08 15:50:19 -0800

    some light styling for the entire app

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -2,10 +2,10 @@ import '../styles/PokeCard.css';
 
 export default function PokeCard({ pokemon }) {
   return (
-    <div>
+    <div className="pokeList">
       {pokemon.map((poke) => (
-        <div key={poke.id}>
-          <p>name: {poke.pokemon}</p>
+        <div key={poke.id} className="pokeCard">
+          <p className="pokeName">name: {poke.pokemon}</p>
           <img src={poke.url_image} />
           <p>type 1: {poke.type_1}</p>
           <p>type 2: {poke.type_2}</p>
