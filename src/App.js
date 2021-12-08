7e823b0348bc3adb5643d7ea2982cbac532bf5ed
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-08 12:45:30 -0800

    grabbed pokemon data from api and looped through names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  //state
+  const [pokemonList, setPokemonList] = useState([]);
+
+  //useEffect hook
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+      const data = await response.json();
+      setPokemonList(data.results);
+    };
+    fetchData();
+  }, []);
+
+  //rendering page
   return (
     <div className="App">
       <h1> Welcome to Pokemon </h1>
+      {pokemonList.map((item) => (
+        <p key={item.id}>{item.pokemon}</p>
+      ))}
     </div>
   );
 }
