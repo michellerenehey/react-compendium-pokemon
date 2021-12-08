1effab0d808bf96129a692f3c5aadd9f45baca34
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-08 15:52:47 -0800

    swapped type 2 for pokebase

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -7,9 +7,9 @@ export default function PokeCard({ pokemon }) {
         <div key={poke.id} className="pokeCard">
           <p className="pokeName">name: {poke.pokemon}</p>
           <img src={poke.url_image} />
-          <p>type 1: {poke.type_1}</p>
-          <p>type 2: {poke.type_2}</p>
+          <p>type: {poke.type_1}</p>
           <p>ability: {poke.ability_1}</p>
+          <p>pokebase: {poke.pokebase}</p>
         </div>
       ))}
     </div>
