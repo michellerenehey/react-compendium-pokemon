97d17272d19937d0638206b13e74f99e885adcb1
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-08 17:17:05 -0800

    updated sort url search parameters

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,7 +1,7 @@
 export async function getPokemon(query, order) {
   const params = new URLSearchParams();
   params.set('pokemon', query);
-  params.set('sort', 'asc');
+  params.set('sort', 'pokemon');
   params.set('direction', order);
   const response = await fetch(
     `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
